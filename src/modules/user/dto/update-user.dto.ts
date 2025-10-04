850354d19339c73a9fcfed6950e3c9511ba916af
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-10-04 12:30:36 +0500

    user service

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,16 +1,9 @@
-import { IsEmail, IsInt, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator"
+import { IsEmail, IsInt, IsOptional, IsString, Matches, MinLength } from "class-validator"
 
 export class UpdateUserDto {
   @IsOptional()
   @IsEmail({}, { message: 'Invalid email format' })
   email?: string
-
-  @IsOptional()
-  @IsString()
-  @MinLength(3)
-  @MaxLength(20)
-  @Matches(/^[a-zA-Z0-9_]+$/)
-  username?: string
 
   @IsOptional()
   @IsString()
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,9 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RedisService } from '../redis/redis.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt'
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -49,4 +50,91 @@ export class UserService {
             throw new ConflictException(`Error occured while creating a user: ${error}`);
         }
     }
+
+    async findOne(id: string) {
+        const user = await this.prisma.user.findUnique({
+            where: { id },
+            include: {
+                document: true
+            }
+        })
+
+        if (!user) throw new NotFoundException(`User with id: ${id} not found.`)
+
+        return user
+    }
+
+    async findByEmail(email: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {email}
+        })
+
+        if (!user) throw new NotFoundException(`User with email: ${email} not found.`)
+
+        return user
+    }
+
+    async update(id: string, dto: UpdateUserDto) {
+        try {
+            if (!dto) throw new BadRequestException('No data provided for the update.')
+
+            const currentUser = await this.prisma.user.findUnique({
+                where: {id},
+                select: {
+                    version: true,
+                    password: true
+                }
+            })
+
+            if (!currentUser) throw new NotFoundException(`User with id: ${id} not found.`)
+
+            if (dto.version === undefined) {
+                throw new BadRequestException('Version number is required for optimistic locking.')
+            }
+
+            if (dto.version !== currentUser?.version) {
+                throw new ConflictException('User was modified by another process.')
+            }
+
+            let data= { ...dto }
+
+
+            if (dto.password) {
+                data = { ...dto, password: await bcrypt.hash(dto.password, this.SALT_ROUNDS)}
+            }
+            
+            const updatedUser = await this.prisma.$transaction(
+                async (tx) => {
+                    return await tx.user.update({
+                        where: {
+                            id,
+                            version: currentUser.version
+                        },
+                        data: {
+                            ...data,
+                            version: { increment: 1 }
+                        },
+                        select: {
+                            id: true,
+                            email: true,
+                            version: true,
+                            updatedAt: true
+                        }
+                    })
+                },
+                {
+                    isolationLevel: 'ReadCommitted'
+                }
+            )
+
+            return updatedUser
+
+        } catch (error) {
+            throw new Error(`Error occured while updateing user data: ${error}`)
+        }
+    }
+
+    //softdelete
+
+    //harddelete
 }
